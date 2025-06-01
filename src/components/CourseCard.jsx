54db9f379b00317fff85ffaf713d169b3079ec92
Danish Hussain <danishd.ug21.cs@nitp.ac.in>
2025-06-01 22:45:15 +0530

    Training tab UI

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,32 +1,67 @@
 import React from "react";
 import { useEffect } from "react";
 import AOS from "aos";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesRight, faArrowUpRightDots, faBriefcase, faChalkboard, faChalkboardUser, faMagnifyingGlass, faMoneyCheck, faPencil, faUserGroup } from '@fortawesome/free-solid-svg-icons';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
+import { NavLink } from "react-router-dom";
+
+const services = [
+  {
+    title: "Comprehensive Curriculum",
+    desc: "Designed to build industry-valued skills and most-widely used tools and techniques.",
+    icon: faBriefcase,
+  },
+  {
+    title: "Interactive Mentored Learning",
+    desc: "Collaborative yet personalised live online mentored learning in small groups.",
+    icon: faChalkboardUser,
+  },
+  {
+    title: "Hands-on learning",
+    desc: "Become an AI & ML expert with 8+ hands-on projects under the guidance of industry experts.",
+    icon: faMagnifyingGlass,
+  },
+  {
+    title: "Dedicated career support",
+    desc: "Personalised 1:1 career guidance and interview prep with Resume & LinkedIn review by experts.",
+    icon: faChalkboard,
+  },
+  {
+    title: "Program Support & Networking",
+    desc: "Dedicated Program Manager to ensure you stay on track and motivated. Interact with peers from diverse backgrounds during sessions.",
+    icon: faArrowUpRightDots,
+  },
+];
 
 
 const CourseCard = () => {
 
-    useEffect(() => {
-      AOS.init({ duration: 1300 });
-    }, []);
+  useEffect(() => {
+    AOS.init({ duration: 1300 });
+  }, []);
 
   return (
     <div className="w-11/12 mt-12 p-6 sm:w-5/6 lg:w-4/5 max-w-screen-lg mx-auto">
       <div className='font-sans text-blue-900 text-2xl space-y-1' data-aos="fade-up">
-          <h2 className='tracking-wider'>Training Time</h2>
-          <h2 className='font-bold tracking-wide'>Explore Our Courses</h2>
-        </div>
-      <div className="bg-white rounded-xl my-8 shadow-lg overflow-hidden relative border-t-4 border-pink-500" data-aos="fade-up">
+        <h2 className='tracking-wider'>Training Time</h2>
+        <h2 className='font-bold tracking-wide'>Explore Our Courses</h2>
+      </div>
+      <div className="bg-white sticky -top-[300px] z-[1] rounded-xl my-8 shadow-lg overflow-hidden border-t-4 border-pink-500" data-aos="fade-up">
         {/* Sparkle New Button */}
         <div className="absolute -top-3 -right-3 z-10 ">
-          <button className="bg-gradient-to-r from-pink-500 to-purple-500 text-white p-4 py-2.5 m-6 rounded-full font-bold text-sm shadow-lg transform transition-transform duration-100 animate-pulse">
+          <div className="bg-gradient-to-r from-pink-500 to-purple-500 text-white p-3 py-2 m-6 rounded-full font-bold text-md shadow-lg transform transition-transform duration-100 animate-pulse">
             <span className="flex items-center gap-1">
               <span className="text-yellow-300">✨</span>
               New
               <span className="text-yellow-300">✨</span>
             </span>
-          </button>
+          </div>
         </div>
-        
+
         <div className="grid lg:grid-cols-2 gap-8"  >
           {/* Left Content */}
           <div className="p-6 lg:p-8">
@@ -50,15 +85,15 @@ const CourseCard = () => {
             </p>
 
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 mb-8">
-              <div className="bg-gray-50 p-4 rounded-lg">
+              <div className="bg-gray-50 p-3 rounded-lg">
                 <p className="text-gray-500 text-sm mb-1">Type</p>
                 <p className="text-[#D62A91] font-semibold">Advanced Certificate</p>
               </div>
-              <div className="bg-gray-50 p-4 rounded-lg">
+              <div className="bg-gray-50 p-3 rounded-lg">
                 <p className="text-gray-500 text-sm mb-1">Admission Deadline</p>
-                <p className="text-[#D62A91] font-semibold">31-May-2025</p>
+                <p className="text-[#D62A91] font-semibold">15-June-2025</p>
               </div>
-              <div className="bg-gray-50 p-4 rounded-lg">
+              <div className="bg-gray-50 p-3 rounded-lg">
                 <p className="text-gray-500 text-sm mb-1">Duration</p>
                 <p className="text-[#D62A91] font-semibold">5 Months</p>
               </div>
@@ -92,7 +127,7 @@ const CourseCard = () => {
               alt="Generative AI Course"
               className="w-full h-full object-cover rounded-r-xl"
             />
-            <div className="absolute bottom-4 left-4 bg-white bg-opacity-95 px-6 py-3 rounded-lg shadow-lg">
+            <div className="absolute bottom-4 left-4 bg-white bg-opacity-95 px-4 py-2 rounded-lg shadow-lg">
               <div className="flex flex-wrap gap-3 items-center">
                 <span className="text-green-700 font-semibold">100% Online</span>
                 <span className="text-[#D62A91] font-semibold">4 weeks</span>
@@ -101,6 +136,106 @@ const CourseCard = () => {
             </div>
           </div>
         </div>
+      </div>
+
+
+
+
+      <div className="bg-white rounded-xl shadow-lg p-6 mt-8 relative z-0" data-aos="fade-up">
+        {/* Who Is This Program For Section */}
+        <h3 className="text-2xl font-bold mb-4 text-blue-900 text-center">Who Is This Program For?</h3>
+        <ul className="list-none pl-6 space-y-2 text-gray-800 w-[88%] mx-auto">
+          <li className="flex items-start gap-2 py-2"><FontAwesomeIcon icon={faAnglesRight} className="text-[#D62A91] mt-2 mx-4 " /><p><b>Program</b> is designed for <b>forward-thinking individuals </b>and organizations seeking to harness the power of artificial intelligence to drive innovation, efficiency, and impact in their fields.</p>
+          </li>
+          <li className="flex items-start gap-2 py-2"><FontAwesomeIcon icon={faAnglesRight} className="text-[#D62A91] mt-1 mx-4 " /><p><b>Industry professionals and domain experts</b> who are looking to gain practical knowledge of how AI technologies can be integrated into their workflows to enhance decision-making, automate tasks, and optimize processes.</p>
+          </li>
+          <li className="flex items-start gap-2 py-2"><FontAwesomeIcon icon={faAnglesRight} className="text-[#D62A91] mt-1  mx-4 " /><p><b>Business leaders, managers, and executives</b> who want a strategic understanding of AI implementation to drive transformation, improve performance, foster innovation, and gain a competitive edge in today's fast-evolving digital landscape.</p>
+          </li>
+          <li className="flex items-start gap-2 py-2"><FontAwesomeIcon icon={faAnglesRight} className="text-[#D62A91] mt-1  mx-4 " /><p><b>Technology and innovation enthusiasts</b> who recognize AI's potential to revolutionize industries and aspire to lead cutting-edge projects that meet the evolving needs of organizations and consumers alike.</p>
+          </li>
+          <li className="flex items-start gap-2 py-2"><FontAwesomeIcon icon={faAnglesRight} className="text-[#D62A91] mt-1  mx-4 " /><p><b>Consultants, regulators, and policymakers</b> who are guiding organizations through digital transformation or shaping.</p></li>
+        </ul>
+
+
+
+
+
+        <div className="bg-white rounded-xl p-6 mt-8 relative z-0" data-aos="fade-up">
+          <h2 className="text-2xl font-bold mb-10 text-blue-900 text-center">Learning Experience</h2>
+          <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-center">
+            <div>
+              <FontAwesomeIcon icon={faMoneyCheck} className="text-blue-900 mt-1 text-5xl mx-4 py-6" />
+              <p className="text-gray-600 text-sm mb-2">Completion Certificate</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faBriefcase} className="text-blue-900 mt-1 text-5xl mx-4 py-6" />
+              <p className="text-gray-600 text-sm mb-2">Capstone Project</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserGroup} className="text-blue-900 mt-1 text-5xl mx-4 py-6" />
+              <p className="text-gray-600 text-sm mb-2">Immersive Discussion</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faChalkboardUser} className="text-blue-900 mt-1 text-5xl mx-4 py-6" />
+              <p className="text-gray-600 text-sm mb-2">Weekly live office hours with the course facilitator</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faPencil} className="text-blue-900 mt-1 text-5xl mx-4 py-6" />
+              <p className="text-gray-600 text-sm mb-2">Practical, scenario-based assignments</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="text-blue-900 mt-1 text-5xl mx-4 py-6" />
+              <p className="text-gray-600 text-sm mb-2">Real-world examples and case studies</p>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+        <div className="bg-white rounded-xl p-6 mt-8 relative z-0" data-aos="fade-up">
+          <h2 className="text-2xl font-bold mb-10 text-blue-900 text-center">Disigned for working Professionals</h2>
+          <div className="relative mt-8">
+            <Swiper
+              spaceBetween={20}
+              slidesPerView={1.2}
+              breakpoints={{
+                800: { slidesPerView: 2 },
+                950: { slidesPerView: 2.3 },
+                1250: { slidesPerView: 3 },
+              }}
+              loop={true}
+              autoplay={{ delay: 3000, disableOnInteraction: true }}
+              pagination={{ clickable: true }}
+              modules={[Pagination, Autoplay]}
+              className="mt-8"
+            >
+              {services.map((service, index) => (
+                <SwiperSlide key={index} className="flex justify-center">
+                  <div className="bg-white p-2 md:p-4 mb-12 rounded-lg shadow-lg max-w-xs min-h-[300px] md:min-h-[300px] flex flex-col justify-between hover:shadow-xl transition-shadow duration-300 ease-in-out transform hover:scale-[1.03]" data-aos="fade-up">
+
+                    <div className="text-center">
+                      <FontAwesomeIcon icon={service.icon} className="text-[#D62A91] text-center mt-1 text-5xl mx-4 py-6" />
+                      <h3 className="text-xl font-semibold text-gray-800 mt-4">
+                        {service.title}
+                      </h3>
+                      <p className="text-gray-600 text-sm mt-2">{service.desc}</p>
+                    </div>
+
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center w-full">
+          <NavLink to="/contact" className="mx-auto text-center bg-[#D62A91] text-white text-base rounded-md hover:bg-pink-600 active:scale-95 px-4 py-2 my-10 md:mb-8">
+            For more details, talk to our experts
+          </NavLink>
+        </div>
+
       </div>
     </div>
   );
